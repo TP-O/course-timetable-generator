006eb5db9cc94ae4c4d56c45652256a87705b7e3
TP-O <letranphong2k1@gmail.com>
2022-08-07 21:38:01 +0700

    feat: add sign-in page

--- layouts/empty.tsx
+++ layouts/empty.tsx
@@ -1,5 +1,16 @@
 import { LayoutProps } from '@/types'
+import { Stack } from '@mui/material'
 
 export function EmptyLayout({ children }: LayoutProps) {
-  return <>{children}</>
+  return (
+    <Stack
+      sx={{
+        minHeight: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {children}
+    </Stack>
+  )
 }
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,11 +1,84 @@
 import { EmptyLayout } from '@/layouts'
 import { NextPageWithLayout } from '@/types'
+import { Facebook, GitHub, Google } from '@mui/icons-material'
+import { Avatar, Button, Divider, Paper, Stack, TextField, Typography } from '@mui/material'
+import Link from 'next/link'
 
 const Saved: NextPageWithLayout = () => {
   return (
-    <div>
-      <h1>Sign-in page</h1>
-    </div>
+    <Paper
+      elevation={3}
+      sx={{
+        width: {
+          xs: 360,
+          md: 448,
+        },
+        p: 5,
+      }}
+    >
+      <Stack alignItems="center">
+        <Avatar
+          alt="ctg-logo"
+          src="https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png"
+          variant="square"
+          sx={{ width: 28, height: 28 }}
+        />
+
+        <Typography
+          variant="h5"
+          component="h2"
+          sx={{
+            textAlign: 'center',
+            pt: 2,
+          }}
+        >
+          Sign in
+        </Typography>
+
+        <Typography
+          variant="body1"
+          component="span"
+          sx={{
+            textAlign: 'center',
+            pt: 1,
+          }}
+        >
+          Use your system account
+        </Typography>
+      </Stack>
+
+      <Stack spacing={4} sx={{ pt: 3 }}>
+        <TextField name="email" label="Email" placeholder="tpo@gmail.com" />
+
+        <TextField name="password" label="Password" type="password" />
+
+        <Stack direction="row" justifyContent="space-between">
+          <Button sx={{ textTransform: 'none' }}>
+            <Link href="/sign-up">Create account</Link>
+          </Button>
+
+          <Button variant="contained" sx={{ textTransform: 'none' }}>
+            Let&apos;s go
+          </Button>
+        </Stack>
+      </Stack>
+
+      <Divider sx={{ my: 2 }}>Or</Divider>
+
+      <Stack direction="row" justifyContent="space-around">
+        <Button variant="outlined">
+          <Google />
+        </Button>
+
+        <Button variant="outlined">
+          <Facebook />
+        </Button>
+
+        <Button variant="outlined">
+          <GitHub />
+        </Button>
+      </Stack>
+    </Paper>
   )
 }
 
