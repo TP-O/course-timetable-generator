44d6f6ca1c85867bf353cbb80b6676633536e4ce
TP-O <letranphong2k1@gmail.com>
2022-08-08 23:32:34 +0700

    feat: convert index to day of week

--- utils/time.ts
+++ utils/time.ts
@@ -1,3 +1,24 @@
 export function convertToDayOfWeek(index: number) {
-  return 'hello'
+  switch (index) {
+    case 0:
+      return 'Mon'
+
+    case 1:
+      return 'Tue'
+
+    case 2:
+      return 'Wed'
+
+    case 3:
+      return 'Thu'
+
+    case 4:
+      return 'Fri'
+
+    case 5:
+      return 'Sat'
+
+    default:
+      return 'Sun'
+  }
 }
