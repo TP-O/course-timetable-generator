a4996d7323f52b97c7f5d7b0de7c82aebbf30514
TP-O <letranphong2k1@gmail.com>
2022-11-20 12:18:41 +0700

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import { CssBaseline } from '@mui/material'
 import { createEmotionCache, theme } from '@/utils/mui'
 import { AppProvider } from '@/contexts'
 import NextNProgress from 'nextjs-progressbar'
+import Script from 'next/script'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -44,6 +45,21 @@ function MyApp(props: AppPropsWithLayout) {
           </AppProvider>
         </SWRConfig>
       </ThemeProvider>
+
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-MB5441SXQ1"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MB5441SXQ1');
+        `}
+      </Script>
     </CacheProvider>
   )
 }
